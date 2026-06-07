2daeaaa660e8dd6272434422989688b45e441111
aleksandrdelen <delenaleksandr@gmail.com>
2026-06-07 19:00:57 +0200

    Connect MongoDB via MongooseModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ScrapersModule } from './scrapers/scrapers.module';
 import { ProductsModule } from './products/products.module';
@@ -6,6 +7,7 @@ import { LeafletsModule } from './leaflets/leaflets.module';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://localhost:27017/lowca'),
     ScheduleModule.forRoot(), // Setup scheduling for cron scraping jobs
     ScrapersModule,
     ProductsModule,
